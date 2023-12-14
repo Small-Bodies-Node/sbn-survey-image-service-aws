Fix minimum cutout size; RA and Dec must be floats and in units of deg.

src/sbn_sis.py
@@ -61,7 +61,7 @@ class LID:
         return self._lid.split(":")[5]
 
 
-def cutout_handler(lid: str, ra: str, dec: str, size: str) -> fits.HDUList:
+def cutout_handler(lid: str, ra: float, dec: float, size: str) -> fits.HDUList:
     """Entry point for getting image cutouts.
 
 
@@ -70,13 +70,14 @@ def cutout_handler(lid: str, ra: str, dec: str, size: str) -> fits.HDUList:
     lid : LID
         PDS4 logical identifier.
 
-    ra : string
-    dec : string
-        Right ascension and declination in hour angle and degrees, parsable by
-        `astropy.coordinates.SkyCoord`.
+    ra : float
+        Right ascension in units of degrees.
+
+    dec : float
+        Declination in units of degrees.
 
     size : string
-        Cutout size, parsable by `astropy.units.Quantity`.
+        Cutout size, parsable by `astropy.units.Quantity`.  Minimum 1 arcsec.
 
 
     Returns
@@ -87,8 +88,8 @@ def cutout_handler(lid: str, ra: str, dec: str, size: str) -> fits.HDUList:
 
     lid: LID = LID(lid)
 
-    position: SkyCoord = SkyCoord(ra, dec, unit=(u.hourangle, u.deg))
-    _size: u.QuantityInfo = np.minimum(u.Quantity(size), 15 * u.arcmin)
+    position: SkyCoord = SkyCoord(ra, dec, unit=(u.deg, u.deg))
+    _size: u.Quantity = np.maximum(u.Quantity(size), 1 * u.arcsec)
 
     url: str = lid_to_url(lid)
     response: requests.Response = requests.get(url)
@@ -102,10 +103,7 @@ def cutout_handler(lid: str, ra: str, dec: str, size: str) -> fits.HDUList:
     data[i].header["CTYPE1"] = "RA---TPV"
     data[i].header["CTYPE2"] = "DEC--TPV"
     with warnings.catch_warnings():
-        warnings.simplefilter(
-            "ignore",
-            (fits.verify.VerifyWarning, FITSFixedWarning)
-        )
+        warnings.simplefilter("ignore", (fits.verify.VerifyWarning, FITSFixedWarning))
         wcs: WCS = WCS(data[i].header)
 
     cutout: Cutout2D = Cutout2D(data[i].data, position, _size, wcs=wcs)
@@ -157,7 +155,7 @@ def css_lid_to_url(lid: Union[LID, str]) -> str:
     lid: LID = LID(lid)
 
     base_url: str = f"https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.catalina.survey/{lid.collection}"
-    basename: str = lid.product_id.upper()[:lid.product_id.index(".")]
+    basename: str = lid.product_id.upper()[: lid.product_id.index(".")]
 
     telescope: str
     date: str
@@ -166,9 +164,7 @@ def css_lid_to_url(lid: Union[LID, str]) -> str:
         telescope, date = basename.split("_")[:2]
         YYMonDD = f"{date[2:4]}{mm_to_Mon[date[4:6]]}{date[6:8]}"
     except IndexError:
-        raise ValueError(
-            f"Invalid Catalina Sky Survey PDS4 logical identifier: {lid}."
-        )
+        raise ValueError(f"Invalid Catalina Sky Survey PDS4 logical identifier: {lid}.")
 
     return f"{base_url}/{telescope}/{date[:4]}/{YYMonDD}/{basename}.arch.fz"
 
@@ -183,7 +179,9 @@ def spacewatch_lid_to_url(lid: Union[LID, str]) -> str:
 
     lid: LID = LID(lid)
 
-    base_url: str = "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.spacewatch.survey/data"
+    base_url: str = (
+        "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.spacewatch.survey/data"
+    )
 
     year: str
     month: str
@@ -219,7 +217,9 @@ def neat_geodss_lid_to_url(lid: Union[LID, str]) -> str:
 
     lid: LID = LID(lid)
 
-    base_url: str = "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.neat.survey/data_geodss"
+    base_url: str = (
+        "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.neat.survey/data_geodss"
+    )
 
     basename: str
     directory: str
@@ -239,7 +239,9 @@ def neat_tricam_lid_to_url(lid: Union[LID, str]) -> str:
 
     lid: LID = LID(lid)
 
-    base_url: str = "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.neat.survey/data_tricam"
+    base_url: str = (
+        "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.neat.survey/data_tricam"
+    )
 
     basename: str
     directory: str

src/test_sbn_sis.py
@@ -6,15 +6,23 @@ from sbn_sis import lid_to_url, cutout_handler
 @pytest.mark.parametrize(
     "lid,expected_url",
     [
-        ("urn:nasa:pds:gbo.ast.catalina.survey:data_calibrated:g96_20210402_2b_f5q9m2_01_0001.arch",
-         "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.catalina.survey/data_calibrated/G96/2021/21Apr02/G96_20210402_2B_F5Q9M2_01_0001.arch.fz"),
-        ("urn:nasa:pds:gbo.ast.spacewatch.survey:data:sw_0993_09.01_2003_03_23_09_18_47.001.fits",
-         "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.spacewatch.survey/data/2003/03/23/sw_0993_09.01_2003_03_23_09_18_47.001.fits"),
-        ("urn:nasa:pds:gbo.ast.neat.survey:data_geodss:g19960417_obsdata_960417070119d",
-         "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.neat.survey/data_geodss/g19960417/obsdata/960417070119d.fit.fz"),
-        ("urn:nasa:pds:gbo.ast.neat.survey:data_tricam:p20011120_obsdata_20011120014036d",
-         "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.neat.survey/data_tricam/p20011120/obsdata/20011120014036d.fit.fz"),
-    ]
+        (
+            "urn:nasa:pds:gbo.ast.catalina.survey:data_calibrated:g96_20210402_2b_f5q9m2_01_0001.arch",
+            "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.catalina.survey/data_calibrated/G96/2021/21Apr02/G96_20210402_2B_F5Q9M2_01_0001.arch.fz",
+        ),
+        (
+            "urn:nasa:pds:gbo.ast.spacewatch.survey:data:sw_0993_09.01_2003_03_23_09_18_47.001.fits",
+            "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.spacewatch.survey/data/2003/03/23/sw_0993_09.01_2003_03_23_09_18_47.001.fits",
+        ),
+        (
+            "urn:nasa:pds:gbo.ast.neat.survey:data_geodss:g19960417_obsdata_960417070119d",
+            "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.neat.survey/data_geodss/g19960417/obsdata/960417070119d.fit.fz",
+        ),
+        (
+            "urn:nasa:pds:gbo.ast.neat.survey:data_tricam:p20011120_obsdata_20011120014036d",
+            "https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.neat.survey/data_tricam/p20011120/obsdata/20011120014036d.fit.fz",
+        ),
+    ],
 )
 def test_lid_to_url(lid, expected_url):
     url = lid_to_url(lid)
@@ -23,7 +31,7 @@ def test_lid_to_url(lid, expected_url):
 
 def test_cutout_handler_css():
     lid = "urn:nasa:pds:gbo.ast.catalina.survey:data_calibrated:g96_20210402_2b_f5q9m2_01_0001.arch"
-    hdu = cutout_handler(lid, "12:43:58", "23:55:23", "5 arcsec")
+    hdu = cutout_handler(lid, 190.99166667, 23.92305556, "5 arcsec")
 
     assert np.all(
         hdu[0].data
@@ -43,7 +51,7 @@ def test_cutout_handler_css():
 
 def test_cutout_handler_spacewatch():
     lid = "urn:nasa:pds:gbo.ast.spacewatch.survey:data:sw_1007_38.07_2004_05_20_06_40_15.003.fits"
-    hdu = cutout_handler(lid, "15:55:51.122", "-14:28:24.055", "3 arcsec")
+    hdu = cutout_handler(lid, 238.96300833, -14.47334861, "3 arcsec")
 
     assert np.all(
         hdu[0].data