6d45af670242712d46b73425cb9d927e7f4b76ec
Michael S. P. Kelley <msk@astro.umd.edu>
2024-11-06 16:47:17 -0500

    Use Enum to indicate bad format string.

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -7,36 +7,30 @@ from astropy.io import fits
 from sbn_sis import cutout_handler, fits_to_image, ImageFormat
 
 from get_file_name import get_file_name
-# from reconstruct_url import reconstruct_url
-from list_files_in_s3_bucket import list_files_in_s3_bucket
 from set_image_to_s3_cache import set_image_to_s3_cache
 from get_image_from_s3_cache import get_image_from_s3_cache
 
 
 def lambda_handler(event: dict, context):
-
-    image_format: ImageFormat = ImageFormat(
-        event["queryStringParameters"].get("format", "fits").lower()
-    )
-
-    if image_format.value not in ["fits", "jpeg", "png"]:
+    try:
+        image_format: ImageFormat = ImageFormat(
+            event["queryStringParameters"].get("format", "fits").lower()
+        )
+    except ValueError:
         return {
             "statusCode": 400,
-            "body": "Invalid image format. Must be one of: fits, jpeg, png"
+            "body": "Invalid image format. Must be one of: fits, jpeg, png",
         }
 
     # Check for cached image
     cached_filename = get_file_name(event)
-    caching_bucket = os.getenv('S3_CACHE_BUCKET_NAME', None)
+    caching_bucket = os.getenv("S3_CACHE_BUCKET_NAME", None)
     if not caching_bucket:
         return {
             "statusCode": 500,
-            "body": "S3_CACHE_BUCKET_NAME environment variable not set"
+            "body": "S3_CACHE_BUCKET_NAME environment variable not set",
         }
-    cached_file_buffer = get_image_from_s3_cache(
-        caching_bucket,
-        cached_filename
-    )
+    cached_file_buffer = get_image_from_s3_cache(caching_bucket, cached_filename)
     if cached_file_buffer:
         return {
             "headers": {"Content-Type": f"image/{image_format.value}"},
@@ -62,9 +56,7 @@ def lambda_handler(event: dict, context):
 
     mime_type = f"image/{image_format.value}"
 
-    set_image_to_s3_cache(
-        buffer, caching_bucket, cached_filename, mime_type
-    )
+    set_image_to_s3_cache(buffer, caching_bucket, cached_filename, mime_type)
 
     return {
         "headers": {"Content-Type": mime_type},
