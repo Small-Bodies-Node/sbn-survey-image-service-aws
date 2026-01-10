85057539d51d90c411ffbbb726271acd659bfbcc
Michael S. P. Kelley <msk@astro.umd.edu>
2026-01-10 10:04:06 -0500

    Some refactoring of lambda_function

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -17,8 +17,21 @@ class ImageFormat(Enum):
     JPEG: str = "jpeg"
     PNG: str = "png"
 
+    @property
+    def mime_type(self) -> str:
+        return f"image/{self.value}"
+
 
 def lambda_handler(event: dict, context):
+    # ENV variables must be set
+    caching_bucket = os.getenv("S3_CACHE_BUCKET_NAME", None)
+    if not caching_bucket:
+        return {
+            "statusCode": 500,
+            "body": "S3_CACHE_BUCKET_NAME environment variable not set",
+        }
+
+    # Image format must be valid
     try:
         image_format = ImageFormat(
             event["queryStringParameters"].get("format", "fits").lower()
@@ -31,53 +44,40 @@ def lambda_handler(event: dict, context):
 
     # Check for cached image
     cached_filename = get_file_name(event)
-    caching_bucket = os.getenv("S3_CACHE_BUCKET_NAME", None)
-    if not caching_bucket:
-        return {
-            "statusCode": 500,
-            "body": "S3_CACHE_BUCKET_NAME environment variable not set",
-        }
     cached_file_buffer = get_image_from_s3_cache(caching_bucket, cached_filename)
+
     if cached_file_buffer:
-        return {
-            "headers": {
-                "Content-Type": f"image/{image_format.value}",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            },
-            "statusCode": 200,
-            "body": base64.b64encode(cached_file_buffer.getvalue()).decode("utf-8"),
-            "isBase64Encoded": True,
-        }
-
-    # No cached-file found, so fetch from the cutout service
-    hdu = cutout_handler(
-        event["pathParameters"]["lid"],
-        float(event["queryStringParameters"]["ra"]),
-        float(event["queryStringParameters"]["dec"]),
-        event["queryStringParameters"]["size"],
-    )
-
-    buffer: io.BytesIO = io.BytesIO()
-    if image_format == ImageFormat.FITS:
-        hdu.writeto(buffer, output_verify="ignore")
+        # Cached file found
+        content_body = base64.b64encode(cached_file_buffer.getvalue()).decode("utf-8")
     else:
-        image = fits_to_image(hdu)
-        image.save(buffer, format=image_format.value, quality=95)
+        # Cached file not found, so fetch from the cutout service
+        hdu = cutout_handler(
+            event["pathParameters"]["lid"],
+            float(event["queryStringParameters"]["ra"]),
+            float(event["queryStringParameters"]["dec"]),
+            event["queryStringParameters"]["size"],
+        )
 
-    mime_type = f"image/{image_format.value}"
+        buffer: io.BytesIO = io.BytesIO()
+        if image_format == ImageFormat.FITS:
+            hdu.writeto(buffer, output_verify="ignore")
+        else:
+            image = fits_to_image(hdu)
+            image.save(buffer, format=image_format.value, quality=95)
 
-    set_image_to_s3_cache(buffer, caching_bucket, cached_filename, mime_type)
+        content_body = base64.b64encode(buffer.getvalue()).decode("utf-8")
+        set_image_to_s3_cache(
+            buffer, caching_bucket, cached_filename, image_format.mime_type
+        )
 
     return {
         "headers": {
-            "Content-Type": mime_type,
+            "Content-Type": image_format.mime_type,
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
         },
         "statusCode": 200,
-        "body": base64.b64encode(buffer.getvalue()).decode("utf-8"),
+        "body": content_body,
         "isBase64Encoded": True,
     }
